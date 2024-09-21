b3c5eeafe8d1e983a3f8d077dbb2b9a9f7b1259e
Krishna <134902959+krishna9212@users.noreply.github.com>
2024-09-21 13:14:50 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
       <div className="right w-full flex flex-col">
         <div className=" flex flex-col gap-6 md:gap-4 items-center w-full h-4/5">
           <span className=" border-b-2  border-black p-3 font-semibold whitespace-nowrap text-3xl uppercase text-green-500 ">
-            About Us <span className="text-black">UAK</span>
+            About Us <span className="text-black">UKA</span>
           </span>
           <p className="text-sm pt-10 w-[90%] md:w-[70%] capitalize tracking-wide">
             We deal in CCTV cameras, biometric systems, Video Door Phone and
@@ -17,7 +17,7 @@ function About() {
             highly sensitive.
           </p>
           <p className="text-sm w-[90%] md:w-[70%] tracking-wide">
-            At UAK, we excel in both the installation of cutting-edge CCTV
+            At UKA, we excel in both the installation of cutting-edge CCTV
             camera systems and the enhancement of your current surveillance
             infrastructure.
           </p>
@@ -33,7 +33,7 @@ function About() {
             is maximized.
           </p>
           <p className="text-sm w-[90%]  md:w-[70%] capitalize tracking-wide">
-            Trust UAK for all your security needs because
+            Trust UKA for all your security needs because
           </p>
           <p className="text-sm w-[90%]  md:w-[70%] capitalize tracking-wide">
             safeguarding what matters most is a responsibility we take with the
