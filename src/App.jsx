186e55f59b4bc1161ba90a9e5106c2009c2bd0be
krishna9212 <krishnarajput9212@gmail.com>
2024-09-27 10:28:43 +0530

    Implement SEO optimizations: React Helmet, sitemap, robots.txt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Navigation from "./components/Navigation"; // Adjust path if needed
 import LandingPage from "./components/LandingPage"; // Adjust path if needed
 import "./App.css";
@@ -15,17 +16,55 @@ import LoadingPage from "./components/LoadingPage";
 function App() {
   return (
     <>
-      <LoadingPage></LoadingPage> 
+      <Helmet>
+        <title>UAK Technologies - Your Trusted Tech Partner</title>
+        <meta
+          name="description"
+          content="UAK Technologies offers top-notch web development services using React, HTML, CSS, JavaScript, and Tailwind. Contact us for your next project!"
+        />
+        <meta
+          property="og:title"
+          content="UAK Technologies - Your Trusted Tech Partner"
+        />
+        <meta
+          property="og:description"
+          content="UAK Technologies offers top-notch web development services using React, HTML, CSS, JavaScript, and Tailwind. Contact us for your next project!"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ukatechnology.com/" />
+        <meta
+          property="og:image"
+          content="https://ukatechnology.com/./../../images/picsvg_download.svg"
+        />
+        <script type="application/ld+json">
+          {`
+      {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "url": "https://ukatechnology.com",
+        "name": "UAK Technologies",
+        "logo": "https://ukatechnology.com/logo.png",
+        "contactPoint": {
+          "@type": "ContactPoint",
+          "telephone": "9555530011",
+          "contactType": "Customer Service"
+        }
+      }
+    `}
+        </script>
+        {/* <!-- Add more meta tags as needed --> */}
+      </Helmet>
+      <LoadingPage />
       <Navigation />
       <LandingPage />
-      <WhyUs></WhyUs>
-      <Calling></Calling>
-      <Products></Products>
-      <WhyChooseUs></WhyChooseUs>
-      <Testimonials></Testimonials>
-      <About></About>
-      <ContactUs></ContactUs>
-      <FooterComponent></FooterComponent>
+      <WhyUs />
+      <Calling />
+      <Products />
+      <WhyChooseUs />
+      <Testimonials />
+      <About />
+      <ContactUs />
+      <FooterComponent />
     </>
   );
 }
